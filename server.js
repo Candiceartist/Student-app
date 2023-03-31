6d06eecda4f32c4cce6a8cd631462fbb0f4905d1
Candice Jovon Juarez <candice.artist@gmail.com>
2023-03-31 13:54:52 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.set('view engine', 'jsx');
+app.engine("jsx", require('express-react-views').createEngine());
 
-const pokemon = require('./models/pokemon.js')
+const pokemon = require('./models/pokemon.js');
+
+
+
+
 
 app.get("/", (req,res) => {
     res.send('<h1>Welcome to the Pokemon App!</h1>');
 });
-app.get("/pokemon", (req,res) => {
-    res.send(pokemon)
-})
+
+app.get('/pokemon', (req, res) => {
+    res.render("Index");
+});
 
 app.listen(port,() => {
     console.log('Listening on port', port);
